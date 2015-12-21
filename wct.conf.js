1231eaed9bec444df5e01cb32e27c669643c9bd6
Willem Burgers <wa.burgers@gmail.com>
2015-12-21 20:15:46 +0100

    install bower dependencies in travis & test on chrome, because the other browsers fail

--- wct.conf.js
+++ wct.conf.js
@@ -7,23 +7,8 @@ module.exports = {
             browsers: [
 							{
 			          browserName: "chrome",
-			          platform: "OS X 10.9",
-			          version: "40"
-			        },
-							{
-			          browserName: "internet explorer",
 			          platform: "Windows 8.1",
-			          version: "11.0"
-			        },
-							{
-			          browserName: "safari",
-			          platform: "OS X 10.10",
-			          version: "8.0"
-			        },
-							{
-			          browserName: "firefox",
-			          platform: "Windows 7",
-			          version: "27.0"
+			          version: "47"
 			        }
             ]
         }
